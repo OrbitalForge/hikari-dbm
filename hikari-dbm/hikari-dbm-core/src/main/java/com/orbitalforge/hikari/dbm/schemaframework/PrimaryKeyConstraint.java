98a2acf65d8421dd9eab80bd023c434fd1211d69
Travis Sharp <furiousscissors@gmail.com>
2016-11-29 21:53:28 -0800

    PrimaryKeyConstraint Refactor
    PK Constraint now extends UniqueConstraint.

--- hikari-dbm/hikari-dbm-core/src/main/java/com/orbitalforge/hikari/dbm/schemaframework/PrimaryKeyConstraint.java
+++ hikari-dbm/hikari-dbm-core/src/main/java/com/orbitalforge/hikari/dbm/schemaframework/PrimaryKeyConstraint.java
@@ -1,13 +1,10 @@
 package com.orbitalforge.hikari.dbm.schemaframework;
 
-public class PrimaryKeyConstraint extends Constraint {
+public class PrimaryKeyConstraint extends UniqueConstraint {
 	public static final String CONSTRAINT_TYPE = "PK";
 
 	@Override
 	public String getConstraintType() {
-		return CONSTRAINT_TYPE;
+		return PrimaryKeyConstraint.CONSTRAINT_TYPE;
 	}
-	
-	public String[] getFields() { return getProperty("fields", new String[]{}); }
-	public void setFields(String... fields) { setProperty("fields", fields); }
 }
